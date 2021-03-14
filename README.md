62c505a94e21c758a34941b25fddcd6f8af47e6c
Ian Dunn <ian@iandunn.name>
2021-03-14 10:31:50 -0700

    static ui

---
+++ README.md
@@ -0,0 +1,16 @@
+# Trailer Mode UX Prototype
+
+a prototype UX for netflix/apple tv/etc -
+
+screenshot
+
+instead of scrolling through horizontal sliders of thumbnails,
+have user click a category of content, and then start playing a list of trailers, 
+and only have to click 1 button to add the show to your watch queue,
+or to move to the next trailer. can hardcode list of trailers on youtube as sample content.
+
+mentino the psychological concept of suficificers vs maximizers.
+this is better for both, but MUCH better for maximizers
+
+
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,155 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
+
+/*
+
+show it in sidebar and it updates real tiem as add content,
+can also x to remove. can click on ilnk to jump back to the title of that entry
+
+ */
+
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [ watchLater, setwatchLater ] = useState( [] );
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+	const trailers = {
+		drama: [
+			{
+				id: 1,
+				name: 'Cherry',
+				trailerId: 'Nt_wyydppg8',
+			},
+
+			{
+				id: 2,
+				name: 'Hala',
+				trailer: 'url/to/trailer.mov',
+			},
+
+			// the banker
+		],
+
+		action: [
+			// the expanse
+			// see
+			// star trek discovery
+		],
+
+		documentaries: [
+			{
+				id: 2,
+				name: 'Long Way Up',
+				trailer: 'url/to/trailer.mov',
+			},
+
+			// boys state
+
+			// tiny world
+		]
+	};
+
+	return (
+		<div className="App">
+			{/* break these into components */}
+
+			<header>
+				<h1>
+					Trailer Mode UX Prototype
+				</h1>
+
+				<p>short desc, see readme for more</p>
+			</header>
+
+			<nav className="category-list">
+				<h2>Categories</h2>
+
+				{/* build automatically from data */}
+
+				<ul>
+					<li>
+						<button data-category="drama">
+							Drama
+						</button>
+					</li>
+
+					<li>
+						<button data-category="action">
+							Action
+						</button>
+					</li>
+
+
+					<li>
+						<button>
+							Documentaries
+						</button>
+					</li>
+
+					<li>
+						<button disabled>
+							Lorum
+						</button>
+					</li>
+
+					<li>
+						<button disabled>
+							Ipsum
+						</button>
+					</li>
+
+					<li>
+						<button disabled>
+							Eleifend Vestibulum
+						</button>
+					</li>
+
+					<li>
+						<button disabled>
+							Attortor
+						</button>
+					</li>
+				</ul>
+			</nav>
+
+			{/* html5 elements instead of all divs, but only when semantic */}
+
+			<main className="trailer-explorer">
+				<iframe
+					width="560"
+					height="315"
+					src="https://www.youtube.com/embed/Nt_wyydppg8"
+					frameborder="0"
+					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+					allowfullscreen
+				></iframe>
+
+				<nav className="trailer-controls">
+					<button>Previous</button>
+					<button>Add to Watch Later</button>
+					<button>Next</button>
+				</nav>
+			</main>
+
+			<aside className="watch-later-list">
+				<h3>Watch Later</h3>
+
+				<ul>
+					<li>
+						<a href="">
+							Foo
+						</a>
+					</li>
+
+					<li>
+						<a href="">
+							Bar
+						</a>
+					</li>
+				</ul>
+			</aside>
+		</div>
+	);
 }
 
-export default App
+export default App;
+
